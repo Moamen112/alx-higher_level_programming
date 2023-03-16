293c4c21481ce2dd113ccf4030e92fa33a514413
Moamen Shehab <memo.shehab30@gmail.com>
2023-03-16 03:54:10 +0200

    Adding scripts

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
-def best_score(a_dictionary):
-    if a_dictionary:
-        my_list = list(a_dictionary.keys())
-        score = 0
-        leader = ""
-        for i in my_list:
-            if a_dictionary[i] > score:
-                score = a_dictionary[i]
-                leader = i
-        return leader
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict.update({key: (value * 2)})
+    return 
