1aa7cfa9226499b22ec6e988c84dbec3330dcdff
Moamen Shehab <memo.shehab30@gmail.com>
2023-04-04 03:15:44 +0200

    Fixing pycodestyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,10 +3,10 @@
 
 
 class Rectangle:
-     """Represent a rectangle class"""
+    """Represent a rectangle class"""
 
     def __init__(self, width=0, height=0):
-         """Initialize a new Rectangle.
+        """Initialize a new Rectangle.
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
