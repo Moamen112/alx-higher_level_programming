56aa926c8204d256da64356d480f4bbb49b02164
Moamen Shehab <memo.shehab30@gmail.com>
2023-04-14 00:16:16 +0200

    Fixing errors

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-"""
-Defines a class Student.
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """Represents a student."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student.
+        """Initialize a new Student.
 
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-        self._first_name = first_name
-        self._last_name = last_name
-        self._age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    def to_json(self, attributes=None):
-        """Returns a dictionary representation of the Student.
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
 
-        If attributes is a list of strings, it represents only those attributes
+        If attributes is a list of strings, represents only those attributes
         included in the list.
 
         Args:
-            attributes (list): (Optional) The attributes to represent.
+            attrs (list): (Optional) The attributes to represent.
         """
-        if isinstance(attributes, list) and all(isinstance(ele, str) for ele in attributes):
-            return {k: getattr(self, k) for k in attributes if hasattr(self, k)}
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
-    def reload_from_json(self, json_data):
-        """Replaces all attributes of the Student.
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
 
         Args:
-            json_data (dict): The key/value pairs to replace attributes with.
+            json: The key/value pairs to replace attributes with.
         """
-        for key, value in json_data.items():
+        for key, value in json.items():
             setattr(self, key, value)
