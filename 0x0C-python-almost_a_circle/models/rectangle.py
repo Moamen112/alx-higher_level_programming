d0e7f68317fb9aade3f860ea5d4586cd28fa1e48
Moamen Shehab <memo.shehab30@gmail.com>
2023-04-18 03:57:22 +0200

    Fixing error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,5 +109,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the string representation of a Rectangle instance"""
-        
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
