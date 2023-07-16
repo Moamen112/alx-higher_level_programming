92f6f05e3ce3ef2ec330ab04e7dd82e062978d65
Moamen Shehab <memo.shehab30@gmail.com>
2023-07-16 14:48:31 +0200

    Fixing error

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import sys
 import requests
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    response = requests.get(url)
-    x_request_id = response.headers.get("X-Request-Id")
-
-    print(x_request_id)
-
+    r = requests.post(url, data=value)
+    print(r.text)
